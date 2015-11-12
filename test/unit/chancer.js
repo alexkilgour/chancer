c64e2ecbc12d2dbcbbd8c3711a6d871b339e0f26
Alex Kilgour <alexjkilgour@gmail.com>
2015-11-12 12:02:38 +0000

    Add more unit tests, improve coverage

--- test/unit/chancer.js
+++ test/unit/chancer.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var assert = require('proclaim');
+var expect = require('chai').expect;
 
 describe('lib/chancer', function () {
     var chancer;
@@ -30,4 +31,92 @@ describe('lib/chancer', function () {
         });
 
     });
+
+    describe('chancer.randomFloat()', function () {
+        var result;
+
+        beforeEach(function () {
+            result = chancer.randomFloat(1,10);
+        });
+
+        it('should return a floating-point number between 1 and 10', function () {
+            assert.greaterThanOrEqual(result, 1);
+            assert.lessThan(result, 10);
+        });
+
+    });
+
+    describe('chancer.randomInt()', function () {
+        var result;
+
+        beforeEach(function () {
+            result = chancer.randomInt(1,10);
+        });
+
+        it('should return a integer number between 1 and 10', function () {
+            assert.greaterThanOrEqual(result, 1);
+            assert.lessThanOrEqual(result, 10);
+        });
+
+    });
+
+    describe('chancer.coinToss()', function () {
+        var result;
+
+        beforeEach(function () {
+            result = chancer.coinToss();
+        });
+
+        it('should return a 0 or 1 to represent a coin toss', function () {
+            assert.include([0,1], result);
+        });
+
+    });
+
+    describe('chancer.fromArray()', function () {
+        var result;
+
+        beforeEach(function () {
+            result = chancer.fromArray([0,1,2,3,4,5]);
+        });
+
+        it('should return a random item from an array', function () {
+            assert.include([0,1,2,3,4,5], result);
+        });
+
+    });
+
+    describe('chancer.shuffleArray()', function () {
+        var result;
+        var expected;
+
+        beforeEach(function () {
+            result = chancer.shuffleArray([0,1,2,3,4,5]);
+            expected = [0,1,2,3,4,5];
+        });
+
+        it('should return an array shuffled into a random order', function () {
+            assert.isArray(result);
+            expect(result).to.deep.have.same.members(expected);
+            assert.notStrictEqual(result, expected);
+        });
+
+    });
+
+    describe('chancer.randomArray()', function () {
+        var result;
+        var expected;
+
+        beforeEach(function () {
+            result = chancer.randomArray(0,10);
+            expected = [0,1,2,3,4,5,6,7,8,9,10];
+        });
+
+        it('should return an array of integers between 0 and 10 (inclusive)', function () {
+            assert.isArray(result);
+            expect(result).to.deep.have.same.members(expected);
+        });
+
+    });
+
 });
