18f759aece208b9220580067f65d0fad79ebedee
Alex Kilgour <alexjkilgour@gmail.com>
2016-01-07 12:38:41 +0000

    Add mersenne twister as method of random generation

--- build/test.js
+++ build/test.js
@@ -1,7 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+var MersenneTwister = require('mersenne-twister');
+var generator = new MersenneTwister();
+
 var chancer = module.exports = {
+    seed: seed,
     random: random,
     float: float,
     int: int,
@@ -12,25 +16,37 @@ var chancer = module.exports = {
     uuid: uuid
 };
 
-// Returns a floating-point number between 0 and 1
-function random () {
-    return Math.random();
-}
-
-// Returns a random number between <min> (inclusive) and <max> (exclusive)
-function float (min, max) {
-    if (typeof min === 'number' && typeof max === 'number') {
-        return Math.random() * (max - min) + min;
+// Specify a seed in order to get a repeatable random sequence
+function seed (num) {
+    if (typeof num === 'number' && (num % 1) === 0) {
+        // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        generator.init_seed(num);
+        return true;
     }
     else {
         return undefined;
     }
 }
 
-// Returns a random integer between <min> (inclusive) and <max> (inclusive)
+// Returns a floating-point number between 0 and 1
+function random () {
+    return generator.random();
+}
+
+// Returns a floating-point number between <min> (inclusive) and <max> (exclusive)
+function float (min, max) {
+    if (typeof min === 'number' && typeof max === 'number') {
+        return generator.random() * (max - min) + min;
+    }
+    else {
+        return undefined;
+    }
+}
+
+// Returns an integer between <min> (inclusive) and <max> (inclusive)
 function int (min, max) {
     if (typeof min === 'number' && typeof max === 'number') {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(generator.random() * (max - min + 1)) + min;
     }
     else {
         return undefined;
@@ -42,13 +58,13 @@ function int (min, max) {
 function coinToss (heads, tails) {
     var first = (typeof heads !== 'undefined') ? heads : 0;
     var second = (typeof tails !== 'undefined') ? tails : 1;
-    return (Math.floor(Math.random() * 2) === 0) ? first : second;
+    return (Math.floor(generator.random() * 2) === 0) ? first : second;
 }
 
 // Returns a random item from an array
 function fromArray (obj) {
     if (typeof obj !== 'undefined' && obj && obj.constructor === Array) {
-        return obj[Math.floor(Math.random() * obj.length)];
+        return obj[Math.floor(generator.random() * obj.length)];
     }
     else {
         return undefined;
@@ -126,7 +142,7 @@ function uuid () {
     });
 }
 
-},{}],2:[function(require,module,exports){
+},{"mersenne-twister":46}],2:[function(require,module,exports){
 (function (global){
 /*!
  * The buffer module from node.js, for the browser.
@@ -8508,6 +8524,213 @@ Library.prototype.test = function(obj, type) {
 };
 
 },{}],46:[function(require,module,exports){
+/*
+  https://github.com/banksean wrapped Makoto Matsumoto and Takuji Nishimura's code in a namespace
+  so it's better encapsulated. Now you can have multiple random number generators
+  and they won't stomp all over eachother's state.
+  
+  If you want to use this as a substitute for Math.random(), use the random()
+  method like so:
+  
+  var m = new MersenneTwister();
+  var randomNumber = m.random();
+  
+  You can also call the other genrand_{foo}() methods on the instance.
+ 
+  If you want to use a specific seed in order to get a repeatable random
+  sequence, pass an integer into the constructor:
+ 
+  var m = new MersenneTwister(123);
+ 
+  and that will always produce the same random sequence.
+ 
+  Sean McCullough (banksean@gmail.com)
+*/
+ 
+/* 
+   A C-program for MT19937, with initialization improved 2002/1/26.
+   Coded by Takuji Nishimura and Makoto Matsumoto.
+ 
+   Before using, initialize the state by using init_seed(seed)  
+   or init_by_array(init_key, key_length).
+ 
+   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
+   All rights reserved.                          
+ 
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+ 
+     1. Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+ 
+     2. Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+ 
+     3. The names of its contributors may not be used to endorse or promote 
+        products derived from this software without specific prior written 
+        permission.
+ 
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ 
+   Any feedback is very welcome.
+   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
+*/
+ 
+var MersenneTwister = function(seed) {
+	if (seed == undefined) {
+		seed = new Date().getTime();
+	} 
+
+	/* Period parameters */  
+	this.N = 624;
+	this.M = 397;
+	this.MATRIX_A = 0x9908b0df;   /* constant vector a */
+	this.UPPER_MASK = 0x80000000; /* most significant w-r bits */
+	this.LOWER_MASK = 0x7fffffff; /* least significant r bits */
+
+	this.mt = new Array(this.N); /* the array for the state vector */
+	this.mti=this.N+1; /* mti==N+1 means mt[N] is not initialized */
+
+	this.init_seed(seed);
+}  
+
+/* initializes mt[N] with a seed */
+/* origin name init_genrand */
+MersenneTwister.prototype.init_seed = function(s) {
+	this.mt[0] = s >>> 0;
+	for (this.mti=1; this.mti<this.N; this.mti++) {
+		var s = this.mt[this.mti-1] ^ (this.mt[this.mti-1] >>> 30);
+		this.mt[this.mti] = (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253)
+		+ this.mti;
+		/* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
+		/* In the previous versions, MSBs of the seed affect   */
+		/* only MSBs of the array mt[].                        */
+		/* 2002/01/09 modified by Makoto Matsumoto             */
+		this.mt[this.mti] >>>= 0;
+		/* for >32 bit machines */
+	}
+}
+
+/* initialize by an array with array-length */
+/* init_key is the array for initializing keys */
+/* key_length is its length */
+/* slight change for C++, 2004/2/26 */
+MersenneTwister.prototype.init_by_array = function(init_key, key_length) {
+	var i, j, k;
+	this.init_seed(19650218);
+	i=1; j=0;
+	k = (this.N>key_length ? this.N : key_length);
+	for (; k; k--) {
+		var s = this.mt[i-1] ^ (this.mt[i-1] >>> 30)
+		this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1664525) << 16) + ((s & 0x0000ffff) * 1664525)))
+		+ init_key[j] + j; /* non linear */
+		this.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
+		i++; j++;
+		if (i>=this.N) { this.mt[0] = this.mt[this.N-1]; i=1; }
+		if (j>=key_length) j=0;
+	}
+	for (k=this.N-1; k; k--) {
+		var s = this.mt[i-1] ^ (this.mt[i-1] >>> 30);
+		this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1566083941) << 16) + (s & 0x0000ffff) * 1566083941))
+		- i; /* non linear */
+		this.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
+		i++;
+		if (i>=this.N) { this.mt[0] = this.mt[this.N-1]; i=1; }
+	}
+
+	this.mt[0] = 0x80000000; /* MSB is 1; assuring non-zero initial array */ 
+}
+
+/* generates a random number on [0,0xffffffff]-interval */
+/* origin name genrand_int32 */
+MersenneTwister.prototype.random_int = function() {
+	var y;
+	var mag01 = new Array(0x0, this.MATRIX_A);
+	/* mag01[x] = x * MATRIX_A  for x=0,1 */
+
+	if (this.mti >= this.N) { /* generate N words at one time */
+		var kk;
+
+		if (this.mti == this.N+1)  /* if init_seed() has not been called, */
+			this.init_seed(5489);  /* a default initial seed is used */
+
+		for (kk=0;kk<this.N-this.M;kk++) {
+			y = (this.mt[kk]&this.UPPER_MASK)|(this.mt[kk+1]&this.LOWER_MASK);
+			this.mt[kk] = this.mt[kk+this.M] ^ (y >>> 1) ^ mag01[y & 0x1];
+		}
+		for (;kk<this.N-1;kk++) {
+			y = (this.mt[kk]&this.UPPER_MASK)|(this.mt[kk+1]&this.LOWER_MASK);
+			this.mt[kk] = this.mt[kk+(this.M-this.N)] ^ (y >>> 1) ^ mag01[y & 0x1];
+		}
+		y = (this.mt[this.N-1]&this.UPPER_MASK)|(this.mt[0]&this.LOWER_MASK);
+		this.mt[this.N-1] = this.mt[this.M-1] ^ (y >>> 1) ^ mag01[y & 0x1];
+
+		this.mti = 0;
+	}
+
+	y = this.mt[this.mti++];
+
+	/* Tempering */
+	y ^= (y >>> 11);
+	y ^= (y << 7) & 0x9d2c5680;
+	y ^= (y << 15) & 0xefc60000;
+	y ^= (y >>> 18);
+
+	return y >>> 0;
+}
+
+/* generates a random number on [0,0x7fffffff]-interval */
+/* origin name genrand_int31 */
+MersenneTwister.prototype.random_int31 = function() {
+	return (this.random_int()>>>1);
+}
+
+/* generates a random number on [0,1]-real-interval */
+/* origin name genrand_real1 */
+MersenneTwister.prototype.random_incl = function() {
+	return this.random_int()*(1.0/4294967295.0); 
+	/* divided by 2^32-1 */ 
+}
+
+/* generates a random number on [0,1)-real-interval */
+MersenneTwister.prototype.random = function() {
+	return this.random_int()*(1.0/4294967296.0); 
+	/* divided by 2^32 */
+}
+
+/* generates a random number on (0,1)-real-interval */
+/* origin name genrand_real3 */
+MersenneTwister.prototype.random_excl = function() {
+	return (this.random_int() + 0.5)*(1.0/4294967296.0); 
+	/* divided by 2^32 */
+}
+
+/* generates a random number on [0,1) with 53-bit resolution*/
+/* origin name genrand_res53 */
+MersenneTwister.prototype.random_long = function() { 
+	var a=this.random_int()>>>5, b=this.random_int()>>>6; 
+	return(a*67108864.0+b)*(1.0/9007199254740992.0); 
+} 
+
+/* These real versions are due to Isaku Wada, 2002/01/09 added */
+
+module.exports = MersenneTwister;
+
+},{}],47:[function(require,module,exports){
 (function (global){
 (function (root) {
     'use strict';
@@ -9060,13 +9283,14 @@ Library.prototype.test = function(obj, type) {
 }(this));
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"util":9}],47:[function(require,module,exports){
+},{"util":9}],48:[function(require,module,exports){
 // jscs:disable maximumLineLength
 'use strict';
 
 var assert = require('proclaim');
 var expect = require('chai').expect;
 
+// jshint maxstatements:21
 describe('lib/chancer', function () {
     var chancer;
 
@@ -9076,6 +9300,27 @@ describe('lib/chancer', function () {
 
     it('should be an object', function () {
         assert.isObject(chancer);
+    });
+
+    it('should have a `seed` method', function () {
+        assert.isFunction(chancer.seed);
+    });
+
+    describe('chancer.seed()', function () {
+        it('should return true if a valid integer is passed', function () {
+            var result = chancer.seed(123);
+            assert.strictEqual(result, true);
+        });
+
+        it('should return `undefined` if no arguments specified', function () {
+            var result = chancer.seed();
+            assert.strictEqual(result, undefined);
+        });
+
+        it('should return `undefined` if a non-integer argument is specified', function () {
+            var result = chancer.seed('string');
+            assert.strictEqual(result, undefined);
+        });
     });
 
     it('should have a `random` method', function () {
@@ -9236,4 +9481,4 @@ describe('lib/chancer', function () {
     });
 });
 
-},{"../../lib/chancer":1,"chai":10,"proclaim":46}]},{},[47]);
+},{"../../lib/chancer":1,"chai":10,"proclaim":47}]},{},[48]);
